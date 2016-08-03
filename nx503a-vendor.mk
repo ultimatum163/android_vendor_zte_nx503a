460e8fc6defd225789138982c1044c8382955516
Maegorn <mattia.pellegrino@live.it>
2016-08-03 09:44:46 +0200

    Revert CNE addition

--- nx503a-vendor.mk
+++ nx503a-vendor.mk
@@ -37,7 +37,6 @@ $(call inherit-product, vendor/zte/nx503a/nx503a-vendor-blobs.mk)
 # Prebuilt APKs
 PRODUCT_PACKAGES += \
     com.qualcomm.location\
-    CNEService \
     qcrilmsgtunnel \
     shutdownlistener \
     TimeService
